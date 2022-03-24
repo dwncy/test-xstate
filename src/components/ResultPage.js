4f07fe7388a5b7db84e816c84fd5562395cc95f6
Darwin <darwin.cahyadi@ninjavan.co>
2022-03-25 00:11:10 +0700

    master

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,22 +1,11 @@
 import { useActor } from "@xstate/react"
-import React, { useContext, useRef } from "react"
+import React, { useContext } from "react"
 import { GlobalStateContext } from "../providers/Provider"
 
 export function ResultPage() {
   const { dummyService } = useContext(GlobalStateContext)
   const { send } = dummyService
   const [state] = useActor(dummyService)
-  const inputRef = useRef()
-
-  if (state.matches({ 'Result Page': 'origin hub Modal shown' })) {
-    return (
-      <>
-        <div>Modal origin hub must be filled</div>
-        <button onClick={() => send('noBtn')}>no</button>
-        <button onClick={() => send('yesBtn')}>yes</button>
-      </>
-    )
-  }
 
   if (state.matches({ 'Result Page': 'edit Mode' })) {
     return (
@@ -38,14 +27,7 @@ export function ResultPage() {
       <button onClick={() => send('backFilter')}>
         Back to Filter page
       </button>
-      <input name="originHub" ref={inputRef}></input>
-      <button onClick={() => {
-        if (!inputRef.current.value) {
-          send('showOriginHubModal')
-        } else {
-          send('modify')
-        }
-      }}>
+      <button onClick={() => send('modify')}>
         Modify
       </button>
     </>
